b9ba269cf9f311029b39391563f13b82d37b4a60
Nelli Gorlova <nelli7035@gmail.com>
2024-11-28 05:35:00 +0600

    Added a method for password encryption

---
+++ test2/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
+const app = express();
+const port = 8000;
+
+const password = 'hello';
+
+app.get('/', (req, res) => {
+    res.send('simple page');
+})
+
+app.get('/encode/:text', (req, res) => {
+    const params = req.params.text;
+    const cipher = Vigenere.Cipher(password).crypt(params);
+
+    return res.send(`Your encrypted password: ${cipher} `);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+})
