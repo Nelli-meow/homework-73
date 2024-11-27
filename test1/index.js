bbd2bf1ab5efa87e81be67ff42d19058c64fb08c
Nelli Gorlova <nelli7035@gmail.com>
2024-11-28 03:37:23 +0600

    Added returning text when sending a GET request

--- test1/index.js
+++ test1/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/', (req, res) => {
+    return res.send(`Empty page....`);
+});
+
+app.get('/:text', (req, res) => {
+    return res.send(`Hello ${req.params.text} :3`);
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
